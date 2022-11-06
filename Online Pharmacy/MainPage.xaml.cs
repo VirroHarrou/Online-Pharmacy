97dac5abfb41a904ef788ccd2a3f0b96aed0d23f
VirroHarrou <81008781+VirroHarrou@users.noreply.github.com>
2022-11-06 22:37:33 +0300

    Добавьте файлы проекта.

---
+++ Online Pharmacy/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Online_Pharmacy.Widgets;
+using Online_Pharmacy.Widgets.SecondWidgets;
+using Windows.UI.Xaml.Controls;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace Online_Pharmacy
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            MainWidget.Navigate(typeof(GraphWidget));
+            SecondWidget.Navigate(typeof(ReportWidget));
+        }
+
+        private void HomeTapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            MainWidget.Navigate(typeof(GraphWidget));
+            SecondWidget.Navigate(typeof(ReportWidget));
+        }
+
+        private void CatalogTapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            MainWidget.Navigate(typeof(ListViewWidget));
+            SecondWidget.Navigate(typeof(DescriptionWidget));
+        }
+
+        private void RecieptTapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            MainWidget.Navigate(typeof(RecieptWidget));
+            SecondWidget.Navigate(typeof(RecipetDescriptionWidget));
+        }
+    }
+}
---
+++ Online Pharmacy/Widgets/GraphWidget.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Online_Pharmacy.Widgets
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class GraphWidget : Page
+    {
+        public GraphWidget()
+        {
+            this.InitializeComponent();
+        }
+
+        private void canvasDraw()
+        {
+            Line line = new Line();
+            
+            line.X1 = 0;
+            line.Y1 = canvas.Height / 10;
+            line.X2 = canvas.Width;
+            line.Y2 = canvas.Height / 10;
+
+            canvas.Children.Add(line);
+        }
+
+        private void Grid_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            canvasDraw();
+        }
+    }
+}
